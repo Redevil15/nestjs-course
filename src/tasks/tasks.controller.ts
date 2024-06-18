98a12d7461057a386342c4c10be140d929aa2c88
Redevil15 <brandonf2345@gmail.com>
2024-06-18 16:15:11 -0600

    feat: Add createTaskDto class and update createTask API endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
+import { CreatetaskDto } from './dto/create-task-dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -13,9 +14,8 @@ export class TasksController {
 
     @Post()
     createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
+        @Body() createTaskDto: CreatetaskDto
     ): Task {
-        return this.tasksService.createTask(title, description);
+        return this.tasksService.createTask(createTaskDto);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v1 as uuidv1 } from 'uuid';
+import { CreatetaskDto } from './dto/create-task-dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,8 @@ export class TasksService {
         return this.tasks;
     };
 
-    createTask(title: string, description: string): Task {
+    createTask(createTaskDto: CreatetaskDto): Task {
+        const { title, description } = createTaskDto;
         const task: Task = {
             id: uuidv1(),
             title,
