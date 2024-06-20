f4b58999911bd98435e37dfc4b7d549d37ee5609
Redevil15 <brandonf2345@gmail.com>
2024-06-20 01:48:28 -0600

    refactor: Add User entity and UserRepository for managing user entities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserRepository],
+  exports: [AuthService],
 })
 export class AuthModule {}
