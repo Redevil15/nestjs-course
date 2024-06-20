4ef90bacaaaf5c0064315c34fa38f48f12fce1df
Redevil15 <brandonf2345@gmail.com>
2024-06-20 02:01:38 -0600

    feat: Add signup functionality to AuthController

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
-    
+    async signUp(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = AuthCredentialsDto;
+        const user = new User();
+
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
 }
