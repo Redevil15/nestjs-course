ed91258e7568b4c03c8033069a222195434fc509
Redevil15 <brandonf2345@gmail.com>
2024-06-20 11:19:11 -0600

    feat: Add passport and jwt dependencies for authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    TypeOrmModule.forFeature([User]), 
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN
+      },
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
