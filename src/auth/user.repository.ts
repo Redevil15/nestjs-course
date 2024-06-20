fd5799b209ebb9a987e0c0f2a464a9d6cef43543
Redevil15 <brandonf2345@gmail.com>
2024-06-20 02:17:21 -0600

    feat: Add unique constraint to username field in User entity

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
@@ -7,10 +7,21 @@ import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 export class UserRepository extends Repository<User> {
     async signUp(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = AuthCredentialsDto;
+
+       
+
         const user = new User();
 
         user.username = username;
         user.password = password;
-        await user.save();
+        try {
+            await user.save();
+        } catch (error) {
+            if(error.code === '23505') { // username duplicate
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException()
+            }
+        }
     }
 }
