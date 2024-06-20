5ecf8c33ebec8078c1d10071272222657d245532
Redevil15 <brandonf2345@gmail.com>
2024-06-20 01:18:22 -0600

    refactor: Update TaskStatusValidationPipe to use TaskStatus enum for allowedStatuses

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,7 +1,13 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../task-status.enum";
+
 
 export class TaskStatusValidationPipe implements PipeTransform {
-    readonly allowedStatuses = ['OPEN', 'IN_PROGRESS', 'DONE'];
+    readonly allowedStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ];
     transform(value: any) {
         value = value.toUpperCase();
 
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import { CreatetaskDto } from './dto/create-task-dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -43,11 +44,11 @@ export class TasksController {
         return this.tasksService.deleteTask(id);
     }
 
-    // @Patch('/:id/status')
-    // updateTaskStatus(
-    //     @Param('id') id: string,
-    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus
-    // ): Task {
-    //     return this.tasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
+    ): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
 }
